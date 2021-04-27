abb5cd4cdcab4b29a2eacf486160f77e31cab13f
Yilmer Avila <yilmer0001@hotmail.com>
2021-04-26 23:23:52 -0500

    delete ping pong

--- bin/www.js
+++ bin/www.js
@@ -25,24 +25,19 @@ bot.on('ready', () => {
 bot.on('message', msg => {
     let Text = msg.content;
     if (msg.author.id !== '835049925104369684') {
-        if (msg.content === 'ping') {
-            msg.reply('pong');
-        }else {
-            let params = {
-                SourceLanguageCode: 'auto',
-                TargetLanguageCode: 'en',
-                Text
-            };
-            translate.translateText(params, function (err, data) {
-                if (err) console.log(err, err.stack);
-                else {
-                    if(data.SourceLanguageCode !== 'en' && data.TranslatedText !== Text){
-                        // console.log(data);
-                        msg.channel.send(data.TranslatedText);
-                    }
+        let params = {
+            SourceLanguageCode: 'auto',
+            TargetLanguageCode: 'en',
+            Text
+        };
+        translate.translateText(params, function (err, data) {
+            if (err) console.log(err, err.stack);
+            else {
+                if (data.SourceLanguageCode !== 'en' && data.TranslatedText !== Text) {
+                    msg.channel.send(data.TranslatedText);
                 }
-            });
-        }
+            }
+        });
     }
 });
 
